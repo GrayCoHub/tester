b4986b9de7d863ffbcff368be4859c0558c1156b
GraySmith80 <priority@gci.net>
2019-08-06 19:49:59 -0800

    Updated with Vignettes and seperated functions

--- tester/R/hello.R
+++ tester/R/hello.R
@@ -1,7 +1,7 @@
 #' My Hello World Function
 #'
 #'
-#'@description
+#'
 #'
 #'
 #' @param x The name of the person speaking to
@@ -20,18 +20,4 @@ hello <- function(x) {
   print(paste("Hello ", x, ", this is the world"))
 }
 
-
-#' Title
-#'
-#' @param r
-#' @param x
-#' @param y
-#'
-#' @return
-#' @export
-#'
-#' @examples
-printer = function(r,x,y) {
-  print(paste0("x = ", x))
-}
 
